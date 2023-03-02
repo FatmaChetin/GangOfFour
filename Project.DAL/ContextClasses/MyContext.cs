86eec5931b68ac1c951a1c2aabb615b59f9c813e
Tuğberk Demiralp <tugberk.demiralp@gmail.com>
2023-03-02 13:29:14 +0300

    WinUI.AppConfig Değiştirildi

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
